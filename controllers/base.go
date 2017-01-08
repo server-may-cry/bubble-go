1e7be7834a56c917806c01571504576cfa879c63
Sergey <sopletaev@gmail.com>
2017-01-08 19:15:49 +0300

    fix json request base parser

--- controllers/base.go
+++ controllers/base.go
@@ -8,7 +8,7 @@ type baseRequest struct {
 	AuthKey string `json:"authKey" binding:"required"` // some hash
 	ExtID   string `json:"extId" binding:"required"`   // "123312693841263"
 	SysID   string `json:"sysId" binding:"required"`   // "VK"
-	MsgID   uint64 `json:"msgId"`                      // not required. just for back capability
+	MsgID   uint64 `json:"msgId,string"`               // not required. just for back capability
 }
 
 func getErrBody(err error) gin.H {
